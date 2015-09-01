fed21fba74e69d5975d05cc6d8fd158d9d0a7d86
BrandonRomano <BrandonRRomano@gmail.com>
2015-09-01 16:14:50 -0400

    Adding create single label method

--- callable.sh
+++ callable.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+Ash__import "ghtools"
+
 Ghtools__callable_main(){
     Ghtools__callable_help
 }
@@ -9,5 +11,12 @@ Ghtools__callable_help(){
 }
 
 Ghtools__callable_labels(){
-    Logger__log "TODO -- Populate Labels"
+    Logger__prompt "Input the the repository to add labels (ex, carrot/ash-ghtools): "; read repo
+
+    success=$(Ghtools_create_single_label "$repo" "wow" "ffffff")
+    if [[ "$success" -eq "1" ]]; then
+        echo "Success!"
+    else
+        echo "Failure!"
+    fi
 }
---
+++ lib/ghtools.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##################################################
+# Creates a single label on a repository
+#
+# @param $1: Repository name
+# @param $2: Label name
+# @param $3: Label color
+##################################################
+Ghtools_create_single_label(){
+    # Try to create via POST
+    post_output=$(curl \
+        -s \
+        -H "Authorization: token $GHTOOLS_TOKEN" \
+        -X POST "https://api.github.com/repos/$1/labels" \
+        -d "{\"name\":\"$2\", \"color\":\"$3\"}")
+
+    # Checking if POST worked
+    post_errors=$(echo "$post_output" | grep "\"errors\":")
+    if [[ -z "$post_errors" ]]; then
+        echo "1" # Success, early return
+    fi
+
+    # Update via PATCH
+    patch_output=$(curl \
+        -s \
+        -H "Authorization: token $GHTOOLS_TOKEN" \
+        -X PATCH "https://api.github.com/repos/$1/labels/$2" \
+        -d "{\"name\":\"$2\", \"color\":\"$3\"}")
+
+    # Checking if PATCH worked
+    patch_errors=$(echo "$patch_output" | grep "\"errors\":")
+    if [[ -z "$patch_errors" ]]; then
+        echo "1"
+    else
+        echo "0"
+    fi
+}
