49681b5cbcdb9998f356a46caf4f5176e231e17f
Henry Snopek <hhsnopek@gmail.com>
2016-11-02 17:03:39 -0400

    add if .git is found, use that repo & add -delete:all

--- callable.sh
+++ callable.sh
@@ -28,26 +28,30 @@ Github__callable_labels(){
         return
     fi
 
-        if [[ "$1" == "list" ]]; then
-            ls $Github_label_config_directory
-            exit
-        else
-            repo=$1
-            label=$2
-        fi
+    if [[ "$1" == "list" ]]; then
+        ls $Github_label_config_directory
+        return
+    elif [[ ! $1 =~ (.*)\/(.*) ]]; then
+        repo=""
+        label=$1
+    else
+        repo=$1
+        label=$2
+    fi
+
 
     # Grabbing repo + validating input
-        if [[ "$repo" == "" ]]; then
+    if [[ "$repo" == "" ]]; then
+        if [[ -d .git ]] && [[ $(git config --local --get remote.origin.url) =~ git@github\.com:(.*)\/(.*)\.git ]]; then
+            repo="${BASH_REMATCH[1]}/${BASH_REMATCH[2]}"
+        else
             Logger__prompt "Input the the repository to add labels (ex, carrot/ash-github): "; read repo
             if [[ ! "$repo" =~ .+/.+ ]]; then
-                    Logger__error "Invalid repository format (ex, carrot/ash-github)"
-                    exit
+                Logger__error "Invalid repository format (ex, carrot/ash-github)"
+                exit
             fi
         fi
-
-        if [[ "$label" == "" ]]; then
-            label=$1
-        fi
+    fi
 
     # Handling the config file
     Github__labels_handle_config_file "$repo" "$label" 0
